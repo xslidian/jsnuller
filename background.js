fe3b57ca27eb4adef593fade562dd8f3176a927f
xslidian <xslidian@gmail.com>
2015-10-26 16:42:44 +0800

    pagead2.googlesyndication.com

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 			"*://hm.baidu.com/*.js*",
 			"*://clientstat.duokan.com/*",
 			"*://log.mmstat.com/*",
+			"*://pagead2.googlesyndication.com/*",
 			"*://*.google-analytics.com/*"
 		]
 	},
